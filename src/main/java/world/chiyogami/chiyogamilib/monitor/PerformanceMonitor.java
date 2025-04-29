793983cef14172dc2f60ca93c325fbdffadd98c8
bea4dev <bea0224@outlook.jp>
2025-04-29 14:13:50 +0900

    update

--- src/main/java/world/chiyogami/chiyogamilib/monitor/PerformanceMonitor.java
+++ src/main/java/world/chiyogami/chiyogamilib/monitor/PerformanceMonitor.java
@@ -1,13 +1,11 @@
 package world.chiyogami.chiyogamilib.monitor;
 
-import org.bukkit.World;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class PerformanceMonitor {
     
-    private static Map<World, Long> WORLD_TICK_NANO_TIME_MAP = new ConcurrentHashMap<>();
+    private static Map<String, Long> WORLD_TICK_NANO_TIME_MAP = new ConcurrentHashMap<>();
     
     private static long ALL_WORLD_TICK_NANO_TIME = 0;
     
@@ -18,11 +16,11 @@ public abstract class PerformanceMonitor {
     
     public static long getFullServerTickNanoTime() {return FULL_SERVER_TICK_NANO_TIME;}
     
-    public static Map<World, Long> getWorldTickNanoTimeMap() {return WORLD_TICK_NANO_TIME_MAP;}
+    public static Map<String, Long> getWorldTickNanoTimeMap() {return WORLD_TICK_NANO_TIME_MAP;}
     
     public static void setAllWorldTickNanoTime(long allWorldTickNanoTime) {ALL_WORLD_TICK_NANO_TIME = allWorldTickNanoTime;}
     
     public static void setFullServerTickNanoTime(long fullServerTickNanoTime) {FULL_SERVER_TICK_NANO_TIME = fullServerTickNanoTime;}
     
-    public static void setWorldTickNanoTimeMap(Map<World, Long> worldTickNanoTimeMap) {WORLD_TICK_NANO_TIME_MAP = worldTickNanoTimeMap;}
+    public static void setWorldTickNanoTimeMap(Map<String, Long> worldTickNanoTimeMap) {WORLD_TICK_NANO_TIME_MAP = worldTickNanoTimeMap;}
 }
